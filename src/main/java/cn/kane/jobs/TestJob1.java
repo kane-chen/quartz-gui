cfaa8271b7907589235dc48208ec857771842099
kane.ch <yiying5@gmail.com>
2014-06-30 14:02:46 +0800

    1.add log4j 2.add job-data-map

--- src/main/java/cn/kane/jobs/TestJob1.java
+++ src/main/java/cn/kane/jobs/TestJob1.java
@@ -2,15 +2,18 @@ package cn.kane.jobs;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class TestJob1 extends QuartzJobBean {
 
+	private static final Logger logger = LoggerFactory.getLogger(TestJob1.class) ;
+	
 	@Override
 	protected void executeInternal(JobExecutionContext context)
 			throws JobExecutionException {
-		// TODO Auto-generated method stub
-		System.out.println("TestJob1 execute");
+		logger.info("JOB1-executeInternal");
 		
 	}
 
--- src/main/java/cn/kane/jobs/TestJob2.java
+++ src/main/java/cn/kane/jobs/TestJob2.java
@@ -1,16 +1,28 @@
 package cn.kane.jobs;
 
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class TestJob2 extends QuartzJobBean{
 
+	private static final Logger logger = LoggerFactory.getLogger(TestJob2.class) ;
+	
 	@Override
 	protected void executeInternal(JobExecutionContext context)
 			throws JobExecutionException {
-		System.out.println("TestJob2 executeInternal");
+		logger.info("TestJob2 executeInternal");
+		JobDataMap jobDataMap = context.getJobDetail().getJobDataMap() ;
+		if(null!=jobDataMap){
+			logger.info("DESTPATH-"+jobDataMap.get("destPath"));
+			for(String key : jobDataMap.getKeys()){
+				logger.info("Context JobDataMap:{}-{}",key,jobDataMap.get(key));
+			}
+		}
+		logger.info("JobDetail JobDataMap:{}",jobDataMap);
 	}
-
 
 }
